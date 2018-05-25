252d5e28c7aa4e4cf7b5963bf06763cc5681c6b7
rwtrlw <rwaterlow@gmail.com>
2018-05-25 12:31:18 -0700

    All mobile-first CSS
    All CSS for mobile-first layout. In addition, Autoprefixer, CSS sourcemaps and minification added to the SASS development workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var minify = require('gulp-cssnano');
+var maps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
 
 gulp.task('watch', ['sass'], function() {
@@ -8,7 +12,7 @@ gulp.task('watch', ['sass'], function() {
         server: '.'
     });
 
-    gulp.watch('./scss/*.scss', ['sass'], browserSync.reload);
+    gulp.watch('./scss/**/*.scss', ['sass'], browserSync.reload);
     gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./js/*.js', browserSync.reload);
 
@@ -16,7 +20,22 @@ gulp.task('watch', ['sass'], function() {
 
 gulp.task('sass', function() {
     return gulp.src('./scss/**/*.scss')
+    .pipe(maps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(minify({
+        zindex: false
+    }))
+    .pipe(maps.write('./'))
+    .pipe(rename(function(path) {
+        if(path.extname === '.css') {
+            path.basename += '.min';
+        }
+    }))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
 });
